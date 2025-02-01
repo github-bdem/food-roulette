bc9cac831911b06a2bc2eac96a55338a57acbc11
github-bdem <5677735+github-bdem@users.noreply.github.com>
2025-01-31 21:29:38 -0500

    scaffolds out basic app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import GithubLink from "@components/GithubLink/GithubLink";
-import PizzaMap from "./components/PizzaMap/PizzaMap";
+import GithubLink from "./components/GithubLink";
 
 function App() {
     return (
-        <>
-            <GithubLink />
-            <PizzaMap />
-            <button className="btn">Button</button>
-        </>
+        <div className="flex h-full w-full flex-col">
+            <div className="navbar bg-base-300 flex justify-between p-4 shadow-sm">
+                <GithubLink />
+                <button className="btn btn-primary p-4">Reroll</button>
+            </div>
+            <div className="flex h-full flex-col-reverse md:flex-row">
+                <div className="bg-base-200 h-1/2 md:h-full md:w-[400px]">
+                    SIDE BAR
+                </div>
+                <div className="flex h-1/2 flex-col items-center justify-center md:h-full md:w-full">
+                    MAP
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/components/GithubLink.tsx
+++ src/components/GithubLink.tsx
@@ -2,7 +2,7 @@ import reactLogo from "@assets/logos/GitHub-Mark-64px.png";
 
 function GithubLink() {
     return (
-        <div>
+        <div className="h-7 w-7">
             <a
                 href="https://github.com/github-bdem/goto.pizza"
                 target="_blank"

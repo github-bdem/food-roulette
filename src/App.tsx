9fa49a8ec69112614b721b0334c645328d7832ed
github-bdem <5677735+github-bdem@users.noreply.github.com>
2025-02-12 00:21:07 -0500

    gets filter bar scaffolded out

--- src/App.tsx
+++ src/App.tsx
@@ -46,14 +46,14 @@ function App() {
                     </div>
                 </APIProvider>
             </div>
-            <div className="drawer-side lg:drawer-open">
+            <div className="drawer-side lg:drawer-open h-full">
                 <label
                     htmlFor="my-drawer-4"
                     aria-label="close sidebar"
                     className="drawer-overlay"
                 ></label>
-                <div className="menu bg-base-200 text-base-content min-h-full w-full p-4 md:w-1/2">
-                    <div className="flex flex-row">
+                <div className="bg-base-200 text-base-content h-full w-full md:w-[450px]">
+                    <div className="navbar bg-base-300 navbar-height flex justify-between p-4 shadow-sm">
                         <div>FILTERS</div>
                         <label
                             htmlFor="my-drawer-4"
@@ -62,37 +62,177 @@ function App() {
                             Close
                         </label>
                     </div>
-                    <div>
-                        <div>
-                            <div>FOOD TYPE</div>
-                            <div></div>
-                        </div>
-                        <div>
-                            <div>DISTANCE</div>
-                            <div></div>
-                        </div>
-                        <div>
-                            <div>PRICE RANGE</div>
-                            <div></div>
-                        </div>
-                        <div>
-                            <div>OPEN NOW</div>
-                            <div></div>
-                        </div>
-                        <div>
-                            <div>RESERVATIONS AVAILABLE</div>
-                            <div></div>
-                        </div>
-                        <div>
-                            <div>DELIVERY AVAILABLE</div>
-                            <div></div>
-                        </div>
-                        <div>
-                            <div>STAR RANGE</div>
-                            <div></div>
-                        </div>
+                    <div className="filter-sidebar-height flex h-5 w-full flex-col items-center gap-4 overflow-scroll p-4">
+                        <fieldset className="fieldset bg-base-100 border-base-300 rounded-box w-5/6 border p-4">
+                            <legend className="fieldset-legend">
+                                Food Type
+                            </legend>
+                            <label className="fieldset-label">
+                                <input type="checkbox" className="checkbox" />
+                                Chicken
+                            </label>
+                            <label className="fieldset-label">
+                                <input type="checkbox" className="checkbox" />
+                                Mexican
+                            </label>
+                            <label className="fieldset-label">
+                                <input type="checkbox" className="checkbox" />
+                                Asian
+                            </label>
+                            <label className="fieldset-label">
+                                <input type="checkbox" className="checkbox" />
+                                Pizza
+                            </label>
+                            <label className="fieldset-label">
+                                <input type="checkbox" className="checkbox" />
+                                Salads
+                            </label>
+                            <label className="fieldset-label">
+                                <input type="checkbox" className="checkbox" />
+                                Burgers
+                            </label>
+                            <label className="fieldset-label">
+                                <input type="checkbox" className="checkbox" />
+                                Ramen
+                            </label>
+                            <label className="fieldset-label">
+                                <input type="checkbox" className="checkbox" />
+                                Indian
+                            </label>
+                            <label className="fieldset-label">
+                                <input type="checkbox" className="checkbox" />
+                                Sushi
+                            </label>
+                            <label className="fieldset-label">
+                                <input type="checkbox" className="checkbox" />
+                                Vegan
+                            </label>
+                            <label className="fieldset-label">
+                                <input type="checkbox" className="checkbox" />
+                                American
+                            </label>
+                        </fieldset>
+                        <fieldset className="fieldset bg-base-100 border-base-300 rounded-box w-5/6 border p-4">
+                            <legend className="fieldset-legend">
+                                Max Distance
+                            </legend>
+                            <div className="w-full max-w-xs">
+                                <input
+                                    type="range"
+                                    min={0}
+                                    max="100"
+                                    className="range"
+                                    step="25"
+                                />
+                                <div className="mt-2 flex justify-between px-2.5 text-xs">
+                                    <span>|</span>
+                                    <span>|</span>
+                                    <span>|</span>
+                                    <span>|</span>
+                                    <span>|</span>
+                                </div>
+                                <div className="mt-2 flex justify-between px-2.5 text-xs">
+                                    <span>1 mi</span>
+                                    <span>2 mi</span>
+                                    <span>3 mi</span>
+                                    <span>4 mi</span>
+                                    <span>5 mi</span>
+                                </div>
+                            </div>
+                        </fieldset>
+                        <fieldset className="fieldset bg-base-100 border-base-300 rounded-box w-5/6 border p-4">
+                            <legend className="fieldset-legend">
+                                Other Options
+                            </legend>
+                            <label className="fieldset-label">
+                                <input
+                                    type="checkbox"
+                                    defaultChecked
+                                    className="toggle"
+                                />
+                                Open Now
+                            </label>
+                            <label className="fieldset-label">
+                                <input
+                                    type="checkbox"
+                                    defaultChecked
+                                    className="toggle"
+                                />
+                                Reservations Available
+                            </label>
+                            <label className="fieldset-label">
+                                <input
+                                    type="checkbox"
+                                    defaultChecked
+                                    className="toggle"
+                                />
+                                Delivery Available
+                            </label>
+                        </fieldset>
+                        <fieldset className="fieldset bg-base-100 border-base-300 rounded-box w-5/6 border p-4">
+                            <legend className="fieldset-legend">Rating</legend>
+                            <div className="rating">
+                                <input
+                                    type="radio"
+                                    name="rating-1"
+                                    className="mask mask-star"
+                                    aria-label="1 star"
+                                />
+                                <input
+                                    type="radio"
+                                    name="rating-1"
+                                    className="mask mask-star"
+                                    aria-label="2 star"
+                                />
+                                <input
+                                    type="radio"
+                                    name="rating-1"
+                                    className="mask mask-star"
+                                    aria-label="3 star"
+                                    defaultChecked
+                                />
+                                <input
+                                    type="radio"
+                                    name="rating-1"
+                                    className="mask mask-star"
+                                    aria-label="4 star"
+                                />
+                                <input
+                                    type="radio"
+                                    name="rating-1"
+                                    className="mask mask-star"
+                                    aria-label="5 star"
+                                />
+                            </div>
+                        </fieldset>
+                        <fieldset className="fieldset bg-base-100 border-base-300 rounded-box w-5/6 border p-4">
+                            <legend className="fieldset-legend">
+                                Price Range
+                            </legend>
+                            <div className="rating">
+                                <input
+                                    type="radio"
+                                    name="rating-1"
+                                    className="mask mask-star"
+                                    aria-label="1 star"
+                                />
+                                <input
+                                    type="radio"
+                                    name="rating-1"
+                                    className="mask mask-star"
+                                    aria-label="2 star"
+                                />
+                                <input
+                                    type="radio"
+                                    name="rating-1"
+                                    className="mask mask-star"
+                                    aria-label="3 star"
+                                    defaultChecked
+                                />
+                            </div>
+                        </fieldset>
                     </div>
-                    <div className="flex flex-row-reverse">
+                    <div className="navbar bg-base-300 navbar-height flex justify-between p-4 shadow-sm">
                         <label
                             htmlFor="my-drawer-4"
                             className="drawer-button btn btn-primary mr-4"

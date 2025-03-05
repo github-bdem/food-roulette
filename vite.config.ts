c4201f37b3e6bd3476276a10fa55ef060f727294
github-bdem <5677735+github-bdem@users.noreply.github.com>
2025-03-04 21:51:31 -0500

    sets up vitest basic setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
+/// <reference types="vitest" />
+
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 // TODO: figure out why path.resolve is so busted according to eslint
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import path from 'path'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  resolve: {
-    alias: {
-      "@components": path.resolve(__dirname, './src/components'),
-      "@assets": path.resolve(__dirname, './src/assets'),
+    base: "./",
+    test: {
+        environment: "jsdom",
     },
-  },
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
-})
+    resolve: {
+        alias: {
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@assets": path.resolve(__dirname, "./src/assets"),
+        },
+    },
+    plugins: [react(), tailwindcss(), tsconfigPaths()],
+});

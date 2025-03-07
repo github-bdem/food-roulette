e0c257415791c86b617c965a136156a59ec57d89
github-bdem <5677735+github-bdem@users.noreply.github.com>
2025-03-07 02:18:10 -0500

    fixes globalThis errors in vitest.setup.ts

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -4,7 +4,18 @@ import * as matchers from "@testing-library/jest-dom/matchers";
 
 expect.extend(matchers);
 
-// TODO: Fix this typing issue, its too late and Im just trying to bang out tests right now
+/**
+ * Weird declaration to make vi.useFakeTimers work with testing-library screen
+ *
+ * https://github.com/vitest-dev/vitest/issues/3117
+ * https://stackoverflow.com/questions/59459312/using-globalthis-in-typescript
+ * https://github.com/microsoft/TypeScript/issues/30139
+ */
+declare global {
+    // eslint-disable-next-line no-var
+    var jest: typeof vi;
+}
+
 globalThis.jest = vi;
 
 afterEach(() => {
